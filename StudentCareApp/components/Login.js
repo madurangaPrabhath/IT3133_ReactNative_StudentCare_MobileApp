97347b9299cf105258d5dc25b1897dfc33edf1ea
madurangaPrabhath <maduracourse@gmail.com>
2025-01-06 19:29:00 +0530

    Add styles to Login page

--- StudentCareApp/components/Login.js
+++ StudentCareApp/components/Login.js
@@ -93,3 +93,59 @@ export default function Login() {
     </KeyboardAvoidingView>
   );
 };
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+  },
+  body: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  container: {
+    flexGrow: 1,
+    padding: 20,
+    width: "100%",
+  },
+  footer: {
+    backgroundColor: "#4b0150",
+    width: "100%",
+    color: "#fff",
+    padding: 20,
+    text: {
+      color: "#fff",
+      textAlign: "center",
+    },
+  },
+  form: {
+    marginTop: 20,
+    input: {
+      width: "100%",
+    },
+  },
+  h1: {
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  image: {
+    marginTop: 20,
+    width: "100%",
+    height: 100,
+    resizeMode: "contain",
+  },
+  button: {
+    backgroundColor: "#4b0150",
+    marginTop: 20,
+  },
+  error: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#f4edf7",
+    borderRadius: 5,
+    flexDirection: "row",
+    gap: 6,
+  },
+});
