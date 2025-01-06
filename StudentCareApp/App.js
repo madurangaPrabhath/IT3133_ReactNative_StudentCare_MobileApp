5e67c34a86acf680382d260fcfa963c899f30765
madurangaPrabhath <maduracourse@gmail.com>
2025-01-06 19:14:35 +0530

    Create Home component

--- StudentCareApp/App.js
+++ StudentCareApp/App.js
@@ -4,14 +4,15 @@ import { StyleSheet } from "react-native";
 import { PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+import Home from "./components/Home";
+
 export default function App() {
   return (
     <SafeAreaProvider>
       <PaperProvider>
         <NavigationContainer>
           <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
+            <Home />
           </View>
         </NavigationContainer>
       </PaperProvider>
