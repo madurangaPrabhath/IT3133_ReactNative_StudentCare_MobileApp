be541b0d4e3755e3be87d4d185c4dcac8eb90fa2
madurangaPrabhath <maduracourse@gmail.com>
2025-01-06 19:31:01 +0530

    Edit Home page

--- StudentCareApp/components/Home.js
+++ StudentCareApp/components/Home.js
@@ -1,5 +1,46 @@
-export default function Home() {
-  return(
-    <></>
-  )
-}
+import { useState } from "react";
+import { BottomNavigation, Text } from "react-native-paper";
+
+import Profile from "./Profile";
+import Courses from "./Courses";
+import Subjects from "./Subjects";
+
+export default function Home({ route }) {
+  const { student } = route.params;
+
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    {
+      key: "profile",
+      title: "Profile",
+      focusedIcon: "account",
+      unfocusedIcon: "account-outline",
+    },
+    {
+      key: "courses",
+      title: "Courses",
+      focusedIcon: "book",
+      unfocusedIcon: "book-outline",
+    },
+    {
+      key: "subjects",
+      title: "Subjects",
+      focusedIcon: "book-open",
+      unfocusedIcon: "book-open-outline",
+    },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    profile: () => <Profile student={student} />,
+    courses: () => <Courses student={student} />,
+    subjects: () => <Subjects student={student} />,
+  });
+
+  return (
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
+  );
+};
